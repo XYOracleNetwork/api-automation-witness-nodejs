a391db537020dae5af39568370e6ff064666811f
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-06-21 17:04:31 -0500

    Add Infura as default provider

--- src/Witnesses/getCryptoMarketWitness.ts
+++ src/Witnesses/getCryptoMarketWitness.ts
@@ -1,10 +1,11 @@
-import { getDefaultProvider, Provider } from '@ethersproject/providers'
+import { Provider } from '@ethersproject/providers'
 import { defaultCoins, defaultCurrencies, UniswapPoolContracts, XyoCoinGeckoCryptoMarketWitness, XyoUniswapCryptoMarketWitness } from '@xyo-network/cryptomarket-witness'
 import { XyoWitness } from '@xyo-network/sdk-xyo-client-js'
 
+import { getInfuraProvider } from './getInfuraProvider'
 import { WitnessProvider } from './WitnessProvider'
 
-export const getCryptoMarketWitness: WitnessProvider<Provider> = (provider = getDefaultProvider()): XyoWitness[] => {
+export const getCryptoMarketWitness: WitnessProvider<Provider> = (provider = getInfuraProvider()): XyoWitness[] => {
   return [
     new XyoUniswapCryptoMarketWitness({ pools: UniswapPoolContracts, schema: XyoUniswapCryptoMarketWitness.schema, targetSchema: XyoUniswapCryptoMarketWitness.schema }, provider),
     new XyoCoinGeckoCryptoMarketWitness({ coins: defaultCoins, currencies: defaultCurrencies, schema: XyoCoinGeckoCryptoMarketWitness.schema }),
