8de6320e5799aaf90379d4179a1f4b6d5c456b28
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-06-21 08:45:50 -0500

    Make init sync for easier server startup testing

--- src/Witnesses/WitnessProvider.ts
+++ src/Witnesses/WitnessProvider.ts
@@ -1,3 +1,3 @@
 import { XyoWitness } from '@xyo-network/sdk-xyo-client-js'
 
-export type WitnessProvider<T> = (opts: T) => Promise<XyoWitness[]>
+export type WitnessProvider<T> = (opts: T) => XyoWitness[]
--- src/Witnesses/getCryptoMarketWitness.ts
+++ src/Witnesses/getCryptoMarketWitness.ts
@@ -4,10 +4,9 @@ import { XyoWitness } from '@xyo-network/sdk-xyo-client-js'
 
 import { WitnessProvider } from './WitnessProvider'
 
-export const getCryptoMarketWitness: WitnessProvider<Provider> = (provider = getDefaultProvider()): Promise<XyoWitness[]> => {
-  const witnesses = [
+export const getCryptoMarketWitness: WitnessProvider<Provider> = (provider = getDefaultProvider()): XyoWitness[] => {
+  return [
     new XyoUniswapCryptoMarketWitness({ pools: UniswapPoolContracts, schema: XyoUniswapCryptoMarketWitness.schema, targetSchema: XyoUniswapCryptoMarketWitness.schema }, provider),
     new XyoCoinGeckoCryptoMarketWitness({ coins: defaultCoins, currencies: defaultCurrencies, schema: XyoCoinGeckoCryptoMarketWitness.schema }),
   ]
-  return Promise.resolve(witnesses)
 }
