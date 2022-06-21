606e7c0fed2e9409c28aeb34337d3e4a9aef4182
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-06-21 12:58:59 -0500

    Observe prices

--- src/Jobs/getCryptoMarketWitnessJob.ts
+++ src/Jobs/getCryptoMarketWitnessJob.ts
@@ -1,8 +1,22 @@
+import { getDefaultProvider } from '@ethersproject/providers'
 import { CronJob } from 'cron'
 
 import { Job } from '../Model'
+// import { storeObservation } from '../Network'
+import { getCryptoMarketWitness } from '../Witnesses'
 
-const testJob = new CronJob('* * * * *', () => console.log('Job ran'), null, true, 'America/Los_Angeles')
+const testJob = new CronJob(
+  '* * * * *',
+  async () => {
+    const witnesses = getCryptoMarketWitness(getDefaultProvider())
+    const observations = await Promise.all(witnesses.map((w) => w.observe()))
+    // const storageResults = await Promise.all(observations.map((observation) => storeObservation(observation)))
+    console.log(observations)
+  },
+  null,
+  true,
+  'America/Los_Angeles'
+)
 
 export const getCryptoMarketWitnessJob = (): Job => {
   return testJob
