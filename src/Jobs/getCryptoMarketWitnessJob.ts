b90e0cec9992a39b3e451336128d9c6a975e5fdc
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-06-22 06:59:54 -0500

    Configurable job schedules

--- src/Jobs/getCryptoMarketWitnessJob.ts
+++ src/Jobs/getCryptoMarketWitnessJob.ts
@@ -3,10 +3,12 @@ import { CronJob } from 'cron'
 import { Job } from '../Model'
 import { storeObservation } from '../Network'
 import { getCryptoMarketWitness } from '../Witnesses'
+import { everyMinute } from './CronSchedules'
+
+const schedule = process.env.CRYPTO_MARKET_WITNESS_JOB_SCHEDULE || everyMinute
 
 const cryptoMarketWitnessJob = new CronJob(
-  // '* * * * * *', // every second
-  '* * * * *', // every minute
+  schedule,
   async () => {
     const witnesses = getCryptoMarketWitness()
     const observations = await Promise.all(witnesses.map((w) => w.observe()))
--- src/Jobs/index.ts
+++ src/Jobs/index.ts
@@ -1,1 +1,2 @@
+export * from './CronSchedules'
 export * from './getJobs'
