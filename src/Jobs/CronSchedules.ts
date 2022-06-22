a14490f56dc59d8ee2182a569dfa36321ba72c9b
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-06-22 06:59:54 -0500

    Configurable job schedules

---
+++ src/Jobs/CronSchedules.ts
@@ -0,0 +1,7 @@
+export const everySecond = '* * * * * *'
+export const everyFiveSeconds = '*/5 * * * * *'
+export const everyTenSeconds = '*/10 * * * * *'
+export const everyFifteenSeconds = '*/15 * * * * *'
+export const everyThirtySeconds = '*/30 * * * * *'
+
+export const everyMinute = '* * * * *'
