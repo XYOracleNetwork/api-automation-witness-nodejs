98d955f5e0bad971b18db35587fb41a4b8243d47
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-06-23 18:34:39 -0500

    Static Infura provider instance

--- src/Witnesses/getInfuraProvider.ts
+++ src/Witnesses/getInfuraProvider.ts
@@ -1,12 +1,15 @@
 import { InfuraProvider, Provider } from '@ethersproject/providers'
 
-export const getInfuraProvider = (): Provider => {
-  const projectId = process.env.INFURA_PROJECT_ID
-  const projectSecret = process.env.INFURA_PROJECT_SECRET
-  return projectId && projectSecret
+const projectId = process.env.INFURA_PROJECT_ID
+const projectSecret = process.env.INFURA_PROJECT_SECRET
+const instance =
+  projectId && projectSecret
     ? new InfuraProvider('homestead', {
         projectId,
         projectSecret,
       })
     : new InfuraProvider()
+
+export const getInfuraProvider = (): Provider => {
+  return instance
 }
