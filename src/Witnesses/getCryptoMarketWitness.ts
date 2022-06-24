a40ab1826e7921be1aa28898509b403dfc7b9bb7
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-06-23 22:16:17 -0500

    Rename method to not collide with package methods

--- src/Witnesses/getCryptoMarketWitness.ts
+++ src/Witnesses/getCryptoMarketWitness.ts
@@ -1,10 +1,11 @@
-import { getDefaultProvider, Provider } from '@ethersproject/providers'
+import { Provider } from '@ethersproject/providers'
 import { defaultCoins, defaultCurrencies, XyoCoinGeckoCryptoMarketWitness, XyoUniswapCryptoMarketWitness } from '@xyo-network/cryptomarket-witness'
 import { UniswapPoolContracts, XyoWitness } from '@xyo-network/sdk-xyo-client-js'
 
+import { getProvider } from '../Providers'
 import { WitnessProvider } from './WitnessProvider'
 
-export const getCryptoMarketWitness: WitnessProvider<Provider> = (provider = getDefaultProvider()): XyoWitness[] => {
+export const getCryptoMarketWitness: WitnessProvider<Provider> = (provider = getProvider()): XyoWitness[] => {
   return [
     new XyoUniswapCryptoMarketWitness({ pools: UniswapPoolContracts, schema: XyoUniswapCryptoMarketWitness.schema, targetSchema: XyoUniswapCryptoMarketWitness.schema }, provider),
     new XyoCoinGeckoCryptoMarketWitness({ coins: defaultCoins, currencies: defaultCurrencies, schema: XyoCoinGeckoCryptoMarketWitness.schema }),
