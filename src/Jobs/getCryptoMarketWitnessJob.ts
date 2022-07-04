7224c6385a19162b9cd47fd73196abb799c17be0
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-07-04 10:08:31 -0500

    Change default job scheduling to every minute

--- src/Jobs/getCryptoMarketWitnessJob.ts
+++ src/Jobs/getCryptoMarketWitnessJob.ts
@@ -2,21 +2,20 @@ import { CronJob } from 'cron'
 
 import { Job } from '../Model'
 import { getCryptoMarketPanel } from '../Panels'
-import { everyTenMinutes } from './CronSchedules'
+import { everyMinute } from './CronSchedules'
 
-// const schedule = process.env.CRYPTO_MARKET_WITNESS_JOB_SCHEDULE || everyMinute
+export const cryptoMarketWitnessJobSchedule = process.env.CRYPTO_MARKET_WITNESS_JOB_SCHEDULE || everyMinute
 
-const cryptoMarketWitnessJob = new CronJob(
-  everyTenMinutes,
-  async () => {
-    console.log(`[${new Date()}] Witnessing Crypto Prices`)
-    await getCryptoMarketPanel().report()
-  },
-  null,
-  true,
-  'America/Los_Angeles'
-)
-
-export const getCryptoMarketWitnessJob = (): Job => {
+export const getCryptoMarketWitnessJob = (schedule = cryptoMarketWitnessJobSchedule): Job => {
+  const cryptoMarketWitnessJob = new CronJob(
+    schedule,
+    async () => {
+      console.log(`[${new Date()}] Witnessing Crypto Prices`)
+      await getCryptoMarketPanel().report()
+    },
+    null,
+    true,
+    'America/Los_Angeles'
+  )
   return cryptoMarketWitnessJob
 }
