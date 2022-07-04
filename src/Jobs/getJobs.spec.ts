bd4dad9718965d5ec13a4fa26bdf67a56f2badbb
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-07-04 10:08:31 -0500

    Change default job scheduling to every minute

--- src/Jobs/getJobs.spec.ts
+++ src/Jobs/getJobs.spec.ts
@@ -1,15 +1,22 @@
 jest.useFakeTimers().setSystemTime(Date.now())
 
+import { Job } from '../Model'
 import { getJobs } from './getJobs'
 
 describe('getJobs', () => {
+  let jobs: Job[] = []
+  beforeAll(() => {
+    jobs = getJobs()
+  })
+  afterAll(() => {
+    jobs.map((job) => job.start())
+  })
   it('gets jobs', () => {
-    const jobs = getJobs()
     expect(jobs).toBeTruthy()
     expect(Array.isArray(jobs)).toBeTruthy()
     expect(jobs.length).toBeGreaterThan(0)
   })
   it('runs jobs on schedule', () => {
-    jest.advanceTimersByTime(1000 * 60 * 10 + 1)
+    jest.advanceTimersByTime(1000 * 60 * 1 + 1)
   })
 })
