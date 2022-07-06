13bb65fbe9e4326ac30425c0e6249bb52a1a998f
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-07-06 09:20:50 -0500

    Modularize types

--- src/Model/index.ts
+++ src/Model/index.ts
@@ -1,1 +1,4 @@
+export * from './InfuraProviderConfig'
 export * from './Job'
+export * from './PocketProviderConfig'
+export * from './ProviderOptions'
--- src/Providers/getProvider.ts
+++ src/Providers/getProvider.ts
@@ -1,30 +1,21 @@
 import { getDefaultProvider, Provider } from '@ethersproject/providers'
 
+import { ProviderOptions } from '../Model'
 import { canUseAlchemyProvider, getAlchemyProviderConfig } from './getAlchemyProvider'
 import { canUseEtherscanProvider, getEtherscanProviderConfig } from './getEtherscanProvider'
 import { canUseInfuraProvider, getInfuraProviderConfig } from './getInfuraProvider'
 import { canUsePocketProvider, getPocketProviderConfig } from './getPocketProvider'
 import { providerOmitted } from './ProviderOmitted'
 
-export interface ProviderOptions {
-  alchemy?: string // Alchemy API Token
-  etherscan?: string // Etherscan API Token
-  infura?: string | { projectId: string; projectSecret: string } // INFURA Project ID or { projectId, projectSecret }
-  pocket?: string | { applicationId: string; applicationSecretKey: string } // Pocket Network Application ID or { applicationId, applicationSecretKey }
-  quorum?: number // The number of backends that must agree (default: 2 for mainnet, 1 for testnets)
-}
-
 export const getProvider = (): Provider => {
-  const provider = getFallbackProvider()
+  const provider = getDefaultProvider('homestead', getProviderOptions())
   return provider
 }
 
-const getFallbackProvider = (): Provider => {
+const getProviderOptions = (): ProviderOptions => {
   const alchemy = canUseAlchemyProvider() ? getAlchemyProviderConfig() : providerOmitted
   const etherscan = canUseEtherscanProvider() ? getEtherscanProviderConfig() : providerOmitted
   const infura = canUseInfuraProvider() ? getInfuraProviderConfig() : providerOmitted
   const pocket = canUsePocketProvider() ? getPocketProviderConfig() : providerOmitted
-  const options: ProviderOptions = { alchemy, etherscan, infura, pocket }
-  const provider = getDefaultProvider('homestead', options)
-  return provider
+  return { alchemy, etherscan, infura, pocket }
 }
