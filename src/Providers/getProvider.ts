ca608e33d7034b011cf543c32697fba4bf982531
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-07-06 08:16:25 -0500

    Add Etherscan provider

--- src/Providers/getProvider.ts
+++ src/Providers/getProvider.ts
@@ -1,17 +1,28 @@
 import { getDefaultProvider, Provider } from '@ethersproject/providers'
 
 import { canUseAlchemyProvider, getAlchemyProviderConfig } from './getAlchemyProvider'
-import { canUseInfuraProvider, getInfuraProvider, getInfuraProviderConfig } from './getInfuraProvider'
+import { canUseEtherscanProvider, getEtherscanProviderConfig } from './getEtherscanProvider'
+import { canUseInfuraProvider, getInfuraProviderConfig } from './getInfuraProvider'
 import { providerOmitted } from './ProviderOmitted'
 
+export interface ProviderOptions {
+  alchemy?: string // Alchemy API Token
+  etherscan?: string // Etherscan API Token
+  infura?: string | { projectId: string; projectSecret: string } // INFURA Project ID or { projectId, projectSecret }
+  pocket?: string | { applicationId: string; applicationSecretKey: string } // Pocket Network Application ID or { applicationId, applicationSecretKey }
+  quorum?: number // The number of backends that must agree (default: 2 for mainnet, 1 for testnets)
+}
+
 export const getProvider = (): Provider => {
-  const provider = canUseInfuraProvider() ? getInfuraProvider() : getFallbackProvider()
+  const provider = getFallbackProvider()
   return provider
 }
 
 const getFallbackProvider = (): Provider => {
-  const infura = canUseInfuraProvider() ? getInfuraProviderConfig() : providerOmitted
   const alchemy = canUseAlchemyProvider() ? getAlchemyProviderConfig() : providerOmitted
-  const provider = getDefaultProvider('homestead', { alchemy, infura })
+  const etherscan = canUseEtherscanProvider() ? getEtherscanProviderConfig() : providerOmitted
+  const infura = canUseInfuraProvider() ? getInfuraProviderConfig() : providerOmitted
+  const options: ProviderOptions = { alchemy, etherscan, infura }
+  const provider = getDefaultProvider('homestead', options)
   return provider
 }
