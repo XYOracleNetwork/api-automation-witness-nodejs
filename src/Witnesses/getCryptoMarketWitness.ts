0b0948a3984261fd8557570f39b7dfc24b09f6e8
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-07-08 21:07:56 -0500

    Add additional gas price witnesses

--- src/Witnesses/getCryptoMarketWitness.ts
+++ src/Witnesses/getCryptoMarketWitness.ts
@@ -1,6 +1,12 @@
 import { Provider } from '@ethersproject/providers'
 import { defaultCoins, defaultCurrencies, XyoCoinGeckoCryptoMarketWitness, XyoUniswapCryptoMarketWitness } from '@xyo-network/cryptomarket-witness'
-import { UniswapPoolContracts, XyoEtherchainEthereumGasWitnessV1, XyoEtherchainEthereumGasWitnessV2, XyoWitness } from '@xyo-network/sdk-xyo-client-js'
+import {
+  UniswapPoolContracts,
+  XyoEtherchainEthereumGasWitnessV1,
+  XyoEtherchainEthereumGasWitnessV2,
+  XyoEtherscanEthereumGasWitness,
+  XyoWitness,
+} from '@xyo-network/sdk-xyo-client-js'
 
 import { getProvider } from '../Providers'
 import { WitnessProvider } from './WitnessProvider'
@@ -9,7 +15,7 @@ export const getCryptoMarketWitness: WitnessProvider<Provider> = (provider = get
   return [
     new XyoUniswapCryptoMarketWitness({ pools: UniswapPoolContracts, schema: XyoUniswapCryptoMarketWitness.schema, targetSchema: XyoUniswapCryptoMarketWitness.schema }, provider),
     new XyoCoinGeckoCryptoMarketWitness({ coins: defaultCoins, currencies: defaultCurrencies, schema: XyoCoinGeckoCryptoMarketWitness.schema }),
-    // new XyoEtherscanEthereumGasWitness({ schema: XyoEtherscanEthereumGasWitness.schema }),
+    new XyoEtherscanEthereumGasWitness({ schema: XyoEtherscanEthereumGasWitness.schema }),
     new XyoEtherchainEthereumGasWitnessV1({ schema: XyoEtherchainEthereumGasWitnessV1.schema }),
     new XyoEtherchainEthereumGasWitnessV2({ schema: XyoEtherchainEthereumGasWitnessV2.schema }),
   ]
