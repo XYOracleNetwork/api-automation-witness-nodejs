c87ef5cd109841f6cd0051abecc45a9fd40f2e35
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-07-08 21:10:43 -0500

    Alphabetize witnesses

--- src/Witnesses/getCryptoMarketWitness.ts
+++ src/Witnesses/getCryptoMarketWitness.ts
@@ -13,10 +13,10 @@ import { WitnessProvider } from './WitnessProvider'
 
 export const getCryptoMarketWitness: WitnessProvider<Provider> = (provider = getProvider()): XyoWitness[] => {
   return [
-    new XyoUniswapCryptoMarketWitness({ pools: UniswapPoolContracts, schema: XyoUniswapCryptoMarketWitness.schema, targetSchema: XyoUniswapCryptoMarketWitness.schema }, provider),
     new XyoCoinGeckoCryptoMarketWitness({ coins: defaultCoins, currencies: defaultCurrencies, schema: XyoCoinGeckoCryptoMarketWitness.schema }),
-    new XyoEtherscanEthereumGasWitness({ schema: XyoEtherscanEthereumGasWitness.schema }),
     new XyoEtherchainEthereumGasWitnessV1({ schema: XyoEtherchainEthereumGasWitnessV1.schema }),
     new XyoEtherchainEthereumGasWitnessV2({ schema: XyoEtherchainEthereumGasWitnessV2.schema }),
+    new XyoEtherscanEthereumGasWitness({ schema: XyoEtherscanEthereumGasWitness.schema }),
+    new XyoUniswapCryptoMarketWitness({ pools: UniswapPoolContracts, schema: XyoUniswapCryptoMarketWitness.schema, targetSchema: XyoUniswapCryptoMarketWitness.schema }, provider),
   ]
 }
