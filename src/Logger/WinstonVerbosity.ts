02533995e1854b0b060780c6ba70478b7e0fe376
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-07-09 23:35:53 -0500

    Modularize interfaces

--- src/Logger/WinstonVerbosity.ts
+++ src/Logger/WinstonVerbosity.ts
@@ -1,11 +1,5 @@
-import { LoggerVerbosity } from './LoggerVerbosity'
-
 /**
  * Follows NPM log levels
  * https://docs.npmjs.com/cli/v8/using-npm/logging#loglevel
  */
-export type WinstonVerbosity = 'silent' | 'error' | 'warn' | 'notice' | 'http' | 'timing' | 'info' | 'verbose' | 'silly'
-
-export const toWinstonVerbosity = (loggerVerbosity: LoggerVerbosity): WinstonVerbosity => {
-  return loggerVerbosity === 'debug' ? 'silly' : loggerVerbosity
-}
+export type WinstonVerbosity = 'error' | 'warn' | 'info' | 'http' | 'verbose' | 'debug' | 'silly'
