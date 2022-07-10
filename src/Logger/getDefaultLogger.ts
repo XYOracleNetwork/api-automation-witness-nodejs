2594efbfa7f2f3672749045570fe96b7c0a39e81
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-07-10 14:42:33 -0500

    Methods for default/custom logger

--- src/Logger/getDefaultLogger.ts
+++ src/Logger/getDefaultLogger.ts
@@ -1,20 +1,6 @@
-import { createLogger, transports } from 'winston'
-
-import { logFormatLocalDev, logFormatStructured } from './LogFormats'
+import { getLogger } from './getLogger'
 import { Logger } from './Logger'
-import { LoggerMeta } from './LoggerMeta'
-import { LoggerVerbosity } from './LoggerVerbosity'
-import { toWinstonVerbosity } from './toWinstonVerbosity'
 import { WrappedWinstonLogger } from './WrappedWinstonLogger'
-
-const { Console } = transports
-
-const format = process.env.NODE_ENV === 'production' ? logFormatStructured : logFormatLocalDev
-const transport = new Console()
-
-// TODO: Make dynamic and pass in for re-use
-const defaultAutomationWitnessMeta: LoggerMeta = { service: 'api-automation-witness' }
-const handleRejections = true
 
 /**
  * Static instance to prevent multiple instances of the same logger
@@ -24,17 +10,4 @@ let defaultLogger: WrappedWinstonLogger
 export const getDefaultLogger = (): Logger => {
   if (defaultLogger) return defaultLogger
   return getLogger()
-}
-
-export const getLogger = (minimumVerbosity: LoggerVerbosity = 'info', defaultMeta: LoggerMeta = defaultAutomationWitnessMeta): Logger => {
-  const level = toWinstonVerbosity(minimumVerbosity)
-  const logger = createLogger({
-    defaultMeta,
-    format,
-    handleRejections,
-    level,
-    rejectionHandlers: [transport],
-    transports: [transport],
-  })
-  return new WrappedWinstonLogger(logger)
 }
