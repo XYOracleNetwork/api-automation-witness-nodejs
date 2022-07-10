a5ff5e3c141f0747f8cf2f23e14ec76d24509526
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-07-09 22:01:27 -0500

    Wrap Winston logger with default console methods

---
+++ src/Logger/WinstonVerbosity.ts
@@ -0,0 +1,5 @@
+/**
+ * Follows NPM log levels
+ * https://docs.npmjs.com/cli/v8/using-npm/logging#loglevel
+ */
+export type WinstonVerbosity = 'silent' | 'error' | 'warn' | 'notice' | 'http' | 'timing' | 'info' | 'verbose' | 'silly'
