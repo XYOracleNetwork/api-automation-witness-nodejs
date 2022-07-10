4d84e6ab01cb967194e2233ab53ed92ae5f7ee57
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-07-09 22:38:57 -0500

    Configurable verbosity

--- src/Logger/WinstonVerbosity.ts
+++ src/Logger/WinstonVerbosity.ts
@@ -1,5 +1,11 @@
+import { LoggerVerbosity } from './LoggerVerbosity'
+
 /**
  * Follows NPM log levels
  * https://docs.npmjs.com/cli/v8/using-npm/logging#loglevel
  */
 export type WinstonVerbosity = 'silent' | 'error' | 'warn' | 'notice' | 'http' | 'timing' | 'info' | 'verbose' | 'silly'
+
+export const toWinstonVerbosity = (loggerVerbosity: LoggerVerbosity): WinstonVerbosity => {
+  return loggerVerbosity === 'debug' ? 'silly' : loggerVerbosity
+}
