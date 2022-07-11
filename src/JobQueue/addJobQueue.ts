2e481fd626434df9fbbe93e25eb71f9f1c2d1f1b
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-07-10 20:18:42 -0500

    Rename to job queue

--- src/JobQueue/addJobQueue.ts
+++ src/JobQueue/addJobQueue.ts
@@ -8,11 +8,11 @@ const collection = 'automationWitness'
 export const addJobQueue = (): Agenda => {
   const address = process.env.MONGO_CONNECTION_STRING || 'mongodb://root:example@localhost:27017/job?authSource=admin'
   const db = { address, collection }
-  const agenda = new Agenda({ db })
+  const jobQueue = new Agenda({ db })
 
   // TODO: Depends on job interval
   const jobProcessingInterval = process.env.CRYPTO_MARKET_WITNESS_JOB_PROCESSING_INTERVAL || '20 seconds'
-  agenda.processEvery(jobProcessingInterval)
+  jobQueue.processEvery(jobProcessingInterval)
 
-  return agenda
+  return jobQueue
 }
