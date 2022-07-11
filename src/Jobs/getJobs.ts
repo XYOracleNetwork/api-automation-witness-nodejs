cc26dd37a0c80c029a9e842cc52049d8941289bf
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-07-10 19:51:15 -0500

    Remove legacy jobs

--- src/Jobs/getJobs.ts
+++
@@ -1,6 +0,0 @@
-import { Job } from '../Model'
-import { getCryptoMarketWitnessJob } from './getCryptoMarketWitnessJob'
-
-export const getJobs = (): Job[] => {
-  return [getCryptoMarketWitnessJob()]
-}
--- src/server/addJobs.ts
+++
@@ -1,7 +0,0 @@
-import { Express } from 'express'
-
-import { getJobs } from '../Jobs'
-
-export const addJobs = (app: Express) => {
-  app.locals.jobs = getJobs()
-}
