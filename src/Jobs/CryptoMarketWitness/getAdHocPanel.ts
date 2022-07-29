961e16645ee0edebb3c77735b3655819e78d3838
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-07-28 22:05:31 -0500

    Refactor

---
+++ src/Jobs/CryptoMarketWitness/getAdHocPanel.ts
@@ -0,0 +1,11 @@
+import { XyoAdhocWitness, XyoPanel, XyoPayloadBase } from '@xyo-network/sdk-xyo-client-js'
+
+import { getArchive, getArchivists, getSigningAccount } from '../../Archivists'
+
+export const getAdHocWitness = (prices: XyoPayloadBase): XyoPanel => {
+  const account = getSigningAccount()
+  const archive = getArchive()
+  const archivists = getArchivists()
+  const witnesses = [new XyoAdhocWitness(prices)]
+  return new XyoPanel({ account, archive, archivists, witnesses })
+}
