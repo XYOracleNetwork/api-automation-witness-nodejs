cfd064068a6e30e6a6d3f772a962f462e183dd2c
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-07-28 21:33:01 -0500

    Ensure correct targetSchema

--- src/Witnesses/getCryptoMarketWitness.ts
+++ src/Witnesses/getCryptoMarketWitness.ts
@@ -13,7 +13,9 @@ import { WitnessProvider } from './WitnessProvider'
 
 export const getCryptoMarketWitness: WitnessProvider<Provider> = (provider = getProvider()): XyoWitness[] => {
   const witnesses: XyoWitness[] = [
-    new XyoCoinGeckoCryptoMarketWitness({ query: { coins: defaultCoins, currencies: defaultCurrencies, schema: XyoCoinGeckoCryptoMarketWitness.schema } }),
+    new XyoCoinGeckoCryptoMarketWitness({
+      query: { coins: defaultCoins, currencies: defaultCurrencies, schema: 'network.xyo.crypto.market.coingecko.query', targetSchema: XyoCoinGeckoCryptoMarketWitness.schema },
+    }),
     new XyoEtherchainEthereumGasWitnessV1(),
     new XyoEtherchainEthereumGasWitnessV2(),
     new XyoUniswapCryptoMarketWitness({
@@ -23,7 +25,12 @@ export const getCryptoMarketWitness: WitnessProvider<Provider> = (provider = get
   ]
   if (canUseEtherscanProvider()) {
     const apiKey = getEtherscanProviderConfig()
-    witnesses.push(new XyoEtherscanEthereumGasWitness({ apiKey, query: { schema: 'network.xyo.blockchain.ethereum.gas.etherscan.query' } }))
+    witnesses.push(
+      new XyoEtherscanEthereumGasWitness({
+        apiKey,
+        query: { schema: 'network.xyo.blockchain.ethereum.gas.etherscan.query', targetSchema: 'network.xyo.blockchain.ethereum.gas.etherscan' },
+      })
+    )
   }
   return witnesses
 }
