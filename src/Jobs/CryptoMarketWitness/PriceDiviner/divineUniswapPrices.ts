5fb01306d14bbcfe9de1cbeeb95864893a5ee6ff
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-08-01 18:17:48 -0500

    WIP for uniswap

--- src/Jobs/CryptoMarketWitness/PriceDiviner/divineUniswapPrices.ts
+++ src/Jobs/CryptoMarketWitness/PriceDiviner/divineUniswapPrices.ts
@@ -1,11 +1,15 @@
 import { XyoCryptoMarketUniswapPayload } from '@xyo-network/cryptomarket-witness'
 
+import { XyoCryptoMarketAssetPayload } from '../../../Model'
+
 const xyo = 'xyo'
 const usdt = 'usdt'
 
-export const divineUniswapPrices = (uniswapPayload: XyoCryptoMarketUniswapPayload | undefined) =>
+export const divineUniswapPrices = (uniswapPayload: XyoCryptoMarketUniswapPayload | undefined): XyoCryptoMarketAssetPayload => {
   uniswapPayload?.pairs
     .map((p) => p.tokens)
     .filter((t) => t.some((t) => t.symbol.toLowerCase() === xyo))
     ?.find((t) => t.some((t) => t.symbol.toLowerCase() === usdt))
     ?.find((t) => t.symbol === xyo)?.value
+  throw new Error('')
+}
