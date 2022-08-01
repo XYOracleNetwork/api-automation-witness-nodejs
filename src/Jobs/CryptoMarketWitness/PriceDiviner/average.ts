fd09efff8b6dca47e7de70fd73c6cdb966337dd1
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-08-01 18:36:28 -0500

    Reorder function parameters

--- src/Jobs/CryptoMarketWitness/PriceDiviner/average.ts
+++ src/Jobs/CryptoMarketWitness/PriceDiviner/average.ts
@@ -1,8 +1,20 @@
+import { AssetInfo, XyoCryptoMarketAssetPayload } from '../../../Model'
+
 const isNumber = (val: number | undefined): val is number => {
   return val !== undefined
 }
 
-export const average = (...prices: (number | undefined)[]): number | undefined => {
-  const numbers = prices.filter(isNumber)
+const parseStringifiedNumber = (value: string): number | undefined => {
+  const parsed = parseFloat(value)
+  return isNaN(parsed) ? undefined : parsed
+}
+
+const averageStringifiedNumbers = (...prices: string[]): number | undefined => {
+  const numbers = prices.map(parseStringifiedNumber).filter(isNumber)
   return numbers.length ? numbers.reduce((sum, n) => sum + n, 0) / numbers.length : undefined
+}
+
+export const average = (...payloads: XyoCryptoMarketAssetPayload[]): Record<string, AssetInfo> => {
+  const assets = new Set(payloads.map((payload) => Object.keys(payload.assets)).flatMap((asset) => asset))
+  throw ''
 }
