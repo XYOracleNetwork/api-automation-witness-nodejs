02531bb9fcbc810732f4457e1c58618289aa8570
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-08-02 08:33:15 -0500

    Add test cases

--- src/Jobs/CryptoMarketWitness/PriceDiviner/divinePrices.spec.ts
+++ src/Jobs/CryptoMarketWitness/PriceDiviner/divinePrices.spec.ts
@@ -5,7 +5,10 @@ describe('divinePrices', () => {
   it('divines prices', () => {
     const result = divinePrices(sampleCoinGeckoPayload, sampleUniswapPayload)
     expect(result).toBeObject()
-    expect(result.assets.xyo?.value.usd).toBe('0.014635635000000001')
     expect(result.timestamp).toBeNumber()
+    expect(result.assets.xyo?.value.btc).toBe('6.415295e-7')
+    expect(result.assets.xyo?.value.eth).toBe('0.000008908865')
+    expect(result.assets.xyo?.value.eur).toBe('0.01417995')
+    expect(result.assets.xyo?.value.usd).toBe('0.014635635000000001')
   })
 })
