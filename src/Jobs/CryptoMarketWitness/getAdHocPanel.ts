32b06f1e7b463ba4d640fad82cf90298c5c48372
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-10-20 16:24:46 -0500

    Update panel to match SDK

--- src/Jobs/CryptoMarketWitness/getAdHocPanel.ts
+++ src/Jobs/CryptoMarketWitness/getAdHocPanel.ts
@@ -1,10 +1,30 @@
-import { XyoAdhocWitness, XyoPanel, XyoPayload } from '@xyo-network/sdk-xyo-client-js'
+import {
+  XyoAdhocWitness,
+  XyoModule,
+  XyoModuleResolverFunc,
+  XyoPanel,
+  XyoPanelConfig,
+  XyoPanelConfigSchema,
+  XyoPayload,
+} from '@xyo-network/sdk-xyo-client-js'
 
 import { getArchive, getArchivists } from '../../Archivists'
 
 export const getAdHocPanel = (prices: XyoPayload): XyoPanel => {
+  // TODO: Where to fit archive
   const archive = getArchive()
-  const archivists = getArchivists()
-  const witnesses = [new XyoAdhocWitness(prices)]
-  return new XyoPanel({ archive, witnesses }, archivists[0])
+  const archivistModule = getArchivists()
+  const witnessModule = new XyoAdhocWitness(prices)
+  const modules: XyoModule[] = [...archivistModule, witnessModule]
+  const archivists = ['TODO']
+  const witnesses = ['TODO']
+  const resolver: XyoModuleResolverFunc = (address: string) => {
+    return (modules.find((mod) => mod?.address === address) as XyoModule) || null
+  }
+  const config: XyoPanelConfig = {
+    archivists,
+    schema: XyoPanelConfigSchema,
+    witnesses,
+  }
+  return new XyoPanel(config, undefined, resolver)
 }
