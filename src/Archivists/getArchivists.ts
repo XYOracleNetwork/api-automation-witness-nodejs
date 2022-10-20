7af9e1a0bcb7c1d56d8e7f46dcf1759444b89934
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-10-20 16:24:46 -0500

    Update panel to match SDK

--- src/Archivists/getArchivists.ts
+++ src/Archivists/getArchivists.ts
@@ -1,7 +1,14 @@
-import { PayloadArchivist, XyoApiConfig, XyoArchivistApi, XyoRemoteArchivist, XyoRemoteArchivistConfigSchema } from '@xyo-network/sdk-xyo-client-js'
+import {
+  PayloadArchivist,
+  XyoApiConfig,
+  XyoArchivistApi,
+  XyoModule,
+  XyoRemoteArchivist,
+  XyoRemoteArchivistConfigSchema,
+} from '@xyo-network/sdk-xyo-client-js'
 
 import { getApiConfig } from './getApiConfig'
 
-export const getArchivists = (configs: XyoApiConfig[] = [getApiConfig()]): PayloadArchivist[] => {
+export const getArchivists = (configs: XyoApiConfig[] = [getApiConfig()]): (PayloadArchivist & XyoModule)[] => {
   return configs.map((config) => new XyoRemoteArchivist({ api: new XyoArchivistApi(config), schema: XyoRemoteArchivistConfigSchema }))
 }
