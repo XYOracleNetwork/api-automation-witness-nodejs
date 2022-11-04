52c0b2d1df9196a89c8d6ce46a09ac5d49005b17
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-11-04 12:18:49 -0500

    XyoAccount Address === HD Wallet Address

--- src/Witnesses/getCryptoMarketWitness.hdWallet.spec.ts
+++ src/Witnesses/getCryptoMarketWitness.hdWallet.spec.ts
@@ -1,16 +1,29 @@
-import { HDNode } from '@ethersproject/hdnode'
+import { arrayify } from '@ethersproject/bytes'
+import { HDNode, mnemonicToSeed } from '@ethersproject/hdnode'
+import { computeHmac, SupportedAlgorithm } from '@ethersproject/sha2'
+import { toUtf8Bytes } from '@ethersproject/strings'
 import { XyoAccount } from '@xyo-network/account'
 
-const phrase =
+// "Bitcoin seed"
+const MasterSecret = toUtf8Bytes('Bitcoin seed')
+
+const mnemonic =
   'music snack noble scheme invest off disease pulp mountain sting present uncover steak visual bachelor wait please wreck dwarf lecture car excuse seminar educate'
 
 describe('getCryptoMarketWitness', () => {
   describe('HD Wallet', () => {
     it('can derive HD Wallet', () => {
-      const account = new XyoAccount({ phrase })
+      const seed = mnemonicToSeed(mnemonic)
+      const seedArray: Uint8Array = arrayify(seed)
+      const I: Uint8Array = arrayify(computeHmac(SupportedAlgorithm.sha512, MasterSecret, seedArray))
+      const privateKey = I.slice(0, 32)
+      const account = new XyoAccount({ privateKey })
       expect(account).toBeObject()
-      const hdNode = HDNode.fromMnemonic(phrase)
+      const hdNode = HDNode.fromMnemonic(mnemonic)
       expect(hdNode).toBeObject()
+      const hdWalletAddress = hdNode.address.toLowerCase().replace('0x', '')
+      const xyoWalletAddress = account.addressValue.hex.toLowerCase().replace('0x', '')
+      expect(hdWalletAddress).toEqual(xyoWalletAddress)
     })
   })
 })
