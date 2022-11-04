2a4ca85e533aa984f79a0c43634ebc73d47b6c92
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-11-04 11:53:04 -0500

    Generate wallet from mnemonic

--- src/Witnesses/getCryptoMarketWitness.hdWallet.spec.ts
+++ src/Witnesses/getCryptoMarketWitness.hdWallet.spec.ts
@@ -1,11 +1,15 @@
 import { HDNode } from '@ethersproject/hdnode'
 import { XyoAccount } from '@xyo-network/account'
 
+const phrase =
+  'music snack noble scheme invest off disease pulp mountain sting present uncover steak visual bachelor wait please wreck dwarf lecture car excuse seminar educate'
+
 describe('getCryptoMarketWitness', () => {
   describe('HD Wallet', () => {
     it('can derive HD Wallet', () => {
-      const account = XyoAccount.random()
-      const hdNode: HDNode = new HDNode(undefined, account.private.hex, account.public.hex, '', '', 0, 0, '')
+      const account = new XyoAccount({ phrase })
+      expect(account).toBeObject()
+      const hdNode = HDNode.fromMnemonic(phrase)
       expect(hdNode).toBeObject()
     })
   })
