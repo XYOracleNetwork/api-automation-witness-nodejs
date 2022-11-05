382b433c499fcb82695c4b1e6653df80d3f9f8f2
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-11-04 23:12:40 -0500

    Order address paths sequentially

--- src/Account/WalletPaths.ts
+++ src/Account/WalletPaths.ts
@@ -1,10 +1,10 @@
 export const WalletPaths = {
-  AggregatePricePanel: "m/44'/60'/8'/0/0",
-  CryptoMarketPanel: "m/44'/60'/7'/0/0",
-  XyoCoingeckoCryptoMarketWitness: "m/44'/60'/1'/0/0",
-  XyoCryptoMarketAssetDiviner: "m/44'/60'/2'/0/0",
-  XyoEtherchainEthereumGasWitnessV1: "m/44'/60'/3'/0/0",
-  XyoEtherchainEthereumGasWitnessV2: "m/44'/60'/4'/0/0",
-  XyoEtherscanEthereumGasWitness: "m/44'/60'/5'/0/0",
-  XyoUniswapCryptoMarketWitness: "m/44'/60'/6'/0/0",
+  AggregatePricePanel: "m/44'/60'/1'/0/0",
+  CryptoMarketPanel: "m/44'/60'/2'/0/0",
+  XyoCoingeckoCryptoMarketWitness: "m/44'/60'/3'/0/0",
+  XyoCryptoMarketAssetDiviner: "m/44'/60'/4'/0/0",
+  XyoEtherchainEthereumGasWitnessV1: "m/44'/60'/5'/0/0",
+  XyoEtherchainEthereumGasWitnessV2: "m/44'/60'/6'/0/0",
+  XyoEtherscanEthereumGasWitness: "m/44'/60'/7'/0/0",
+  XyoUniswapCryptoMarketWitness: "m/44'/60'/8'/0/0",
 }
