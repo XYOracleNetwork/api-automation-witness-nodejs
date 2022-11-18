5878320321dd384e41f6bf516a2955f89ddb590d
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-11-18 02:14:30 -0600

    Move AdHoc panel into consumer module

--- src/Jobs/CryptoMarketWitness/AggregatePrices/getCryptoMarketAssetDiviner.ts
+++ src/Jobs/CryptoMarketWitness/AggregatePrices/getCryptoMarketAssetDiviner.ts
@@ -1,6 +1,6 @@
 import { XyoCryptoMarketAssetDiviner, XyoCryptoMarketAssetDivinerConfigSchema } from '@xyo-network/crypto-asset-payload-plugin'
 
-import { getAccount } from '../../Account'
+import { getAccount } from '../../../Account'
 
 export const getCryptoMarketAssetDiviner = async (): Promise<XyoCryptoMarketAssetDiviner> => {
   return await XyoCryptoMarketAssetDiviner.create({
