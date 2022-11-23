bbc6d76c3796e2b57d15127ce015ffcd64c18745
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-11-23 01:29:11 -0600

    Rename modules

--- src/Jobs/EthereumGas/getTask.ts
+++ src/Jobs/EthereumGas/getTask.ts
@@ -3,16 +3,16 @@ import { getDefaultLogger } from '@xylabs/sdk-api-express-ecs'
 import { XyoDivinerWrapper } from '@xyo-network/diviner'
 import { Task } from '@xyo-network/shared'
 
-import { getAggregateEthereumGasPanel } from './getAggregateEthereumGasPanel'
 import { getEthereumGasDiviner } from './getEthereumGasDiviner'
-import { getEthereumGasPanel } from './getEthereumGasPanel'
+import { getEthereumGasDivinerResultPanel } from './getEthereumGasDivinerResultPanel'
+import { getEthereumGasWitnessPanel } from './getEthereumGasWitnessPanel'
 
 export const getTask = (): Task => {
   const logger = getDefaultLogger()
   const task: Task = async () => {
     try {
       logger.log('Witnessing Ethereum Gas')
-      const cryptoMarketPanel = await getEthereumGasPanel()
+      const cryptoMarketPanel = await getEthereumGasWitnessPanel()
       const [, payloads] = await cryptoMarketPanel.report()
       logger.log('Witnessed Ethereum Gas')
       logger.log('Divining Aggregated Gas Price')
@@ -21,7 +21,7 @@ export const getTask = (): Task => {
       const prices = assertEx(answer, 'Empty XyoEthereumGasPayload response from diviner')
       logger.log('Divined Aggregated Gas Price')
       logger.log('Witnessing Aggregated Gas Price')
-      const panel = await getAggregateEthereumGasPanel(prices)
+      const panel = await getEthereumGasDivinerResultPanel(prices)
       await panel.report()
       logger.log('Witnessed Aggregated Gas Price')
     } catch (error) {
