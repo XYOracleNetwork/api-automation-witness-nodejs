63ed49f8f7ac8509566b7c34750dae268409e1f3
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-11-23 00:50:20 -0600

    Get both jobs when registering Jobs with JobQueue

--- src/Jobs/getJobs.ts
+++ src/Jobs/getJobs.ts
@@ -1,6 +1,7 @@
 import { Job } from '../Model'
 import { getJob as getCryptoMarketWitnessJob } from './CryptoMarketWitness'
+import { getJob as getEthereumGasJob } from './EthereumGas'
 
 export const getJobs = (): Job[] => {
-  return [getCryptoMarketWitnessJob()]
+  return [getCryptoMarketWitnessJob(), getEthereumGasJob()]
 }
