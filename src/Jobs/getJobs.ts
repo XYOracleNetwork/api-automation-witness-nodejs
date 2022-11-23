9819f40bf7bdfe1a9932dc95140f43307e564546
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-11-23 00:51:25 -0600

    Rename directory

--- src/Jobs/getJobs.ts
+++ src/Jobs/getJobs.ts
@@ -1,5 +1,5 @@
 import { Job } from '../Model'
-import { getJob as getCryptoMarketWitnessJob } from './CryptoMarketWitness'
+import { getJob as getCryptoMarketWitnessJob } from './CryptoMarket'
 import { getJob as getEthereumGasJob } from './EthereumGas'
 
 export const getJobs = (): Job[] => {
