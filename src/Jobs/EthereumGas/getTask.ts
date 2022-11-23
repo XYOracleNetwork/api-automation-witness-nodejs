8e6472b0af681ef25e9faebf47049c5ebbe8abc6
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-11-23 01:09:17 -0600

    Remove helper method now that code is simplified

--- src/Jobs/EthereumGas/getTask.ts
+++ src/Jobs/EthereumGas/getTask.ts
@@ -1,7 +1,10 @@
+import { assertEx } from '@xylabs/assert'
 import { getDefaultLogger } from '@xylabs/sdk-api-express-ecs'
+import { XyoDivinerWrapper } from '@xyo-network/diviner'
 
 import { Task } from '../../Model'
-import { divineAggregateGas } from './divineAggregateGas'
+import { getAggregateEthereumGasPanel } from './getAggregateEthereumGasPanel'
+import { getEthereumGasDiviner } from './getEthereumGasDiviner'
 import { getEthereumGasPanel } from './getEthereumGasPanel'
 
 export const getTask = (): Task => {
@@ -13,8 +16,14 @@ export const getTask = (): Task => {
       const [, payloads] = await cryptoMarketPanel.report()
       logger.log('Witnessed Ethereum Gas')
       logger.log('Divining Aggregated Gas Price')
-      await divineAggregateGas(payloads)
+      const diviner = await getEthereumGasDiviner()
+      const answer = (await new XyoDivinerWrapper(diviner).divine(payloads)).pop()
+      const prices = assertEx(answer, 'Empty XyoEthereumGasPayload response from diviner')
       logger.log('Divined Aggregated Gas Price')
+      logger.log('Witnessing Aggregated Gas Price')
+      const panel = await getAggregateEthereumGasPanel(prices)
+      await panel.report()
+      logger.log('Witnessed Aggregated Gas Price')
     } catch (error) {
       logger.error(error)
     }
