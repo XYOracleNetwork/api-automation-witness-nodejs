c9af5e43fbf2d70844b1d1c59d3b34190757c8f6
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-11-23 01:07:22 -0600

    Remove helper method now that code is simplified

--- src/Jobs/CryptoMarket/getTask.ts
+++ src/Jobs/CryptoMarket/getTask.ts
@@ -1,7 +1,10 @@
+import { assertEx } from '@xylabs/assert'
 import { getDefaultLogger } from '@xylabs/sdk-api-express-ecs'
+import { XyoDivinerWrapper } from '@xyo-network/diviner'
 
 import { Task } from '../../Model'
-import { divineAggregatePrices } from './divineAggregatePrices'
+import { getAggregatePricePanel } from './getAggregatePricePanel'
+import { getCryptoMarketAssetDiviner } from './getCryptoMarketAssetDiviner'
 import { getCryptoMarketPanel } from './getCryptoMarketPanel'
 
 export const getTask = (): Task => {
@@ -13,8 +16,14 @@ export const getTask = (): Task => {
       const [, payloads] = await cryptoMarketPanel.report()
       logger.log('Witnessed Crypto Prices')
       logger.log('Divining Aggregated Crypto Prices')
-      await divineAggregatePrices(payloads)
+      const diviner = await getCryptoMarketAssetDiviner()
+      const answer = (await new XyoDivinerWrapper(diviner).divine(payloads)).pop()
+      const prices = assertEx(answer, 'Empty XyoCryptoMarketAssetPayload response from diviner')
       logger.log('Divined Aggregated Crypto Prices')
+      logger.log('Witnessing Aggregated Crypto Prices')
+      const panel = await getAggregatePricePanel(prices)
+      await panel.report()
+      logger.log('Witnessed Aggregated Crypto Prices')
     } catch (error) {
       logger.error(error)
     }
